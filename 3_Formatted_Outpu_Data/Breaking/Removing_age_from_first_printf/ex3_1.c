5cbbf0f4ec285821b5921b6c68a5d03505941612
ArturMarekNowak <arturmareknowal@gmail.com>
2020-09-29 10:57:35 +0200

    exercise 3

---
+++ 3_Formatted_Outpu_Data/Breaking/Removing_age_from_first_printf/ex3_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int age = 10;
+	int height = 183;
+
+	printf("I am %d years old\n");
+	printf("I am %d cm high\n", height);
+
+	return 0;
+
+}
