1187087dec598d77456bbe62a4d8caaefd10ac10
ArturMarekNowak <arturmareknowal@gmail.com>
2020-10-28 23:55:15 +0100

    exercise 30 - continuation of exrcise

--- 30_Automated_Testing/tests/minunit.h
+++ 30_Automated_Testing/tests/minunit.h
@@ -9,7 +9,7 @@
 #define mu_suite_start() char *message = NULL
 
 #define mu_assert(test, message) if (!(test)) {\
-	log_error(message); return message; }
+	log_err(message); return message; }
 #define mu_run_test(test) debug("\n-----%s", " " #test); \
 	message = test(); tests_run++; if (message) return message; 
 
